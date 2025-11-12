7bf0b92f9e0b36e16efc3570c78e900380400415
YardenFadida <yardenfadida@gmail.com>
2025-11-12 10:11:57 -0400

    Initial implementation for LR

--- utilities.py
+++ utilities.py
@@ -4,25 +4,15 @@ nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
 from datasets import load_dataset
 
+
 # Tokenizer function using NLTK word_tokenize
 def nltk_tokenizer(text):
     # Preprocess: removes [NAME] tokens 
-    text = re.sub(r'\[NAME\]', '', text)  
-    return word_tokenize(text)
+    text = re.sub(r'\[NAME\]', '', text)
+    tokens = word_tokenize(text)
+    return tokens
 
 
 def retrieve_dataset():
-
     # Load full GoEmotions dataset
-    ds = load_dataset("google-research-datasets/go_emotions")
-
-    # Example: how to access train, dev, and test datasets
-    # train_data = ds['train']
-    # val_data = ds['validation']
-    # test_data = ds['test']
-
-    # Example: how to access texts and labels from set
-    # train_texts = train_data['text']
-    # train_labels = train_data['labels']
-
-    return ds
+    return load_dataset("google-research-datasets/go_emotions")
