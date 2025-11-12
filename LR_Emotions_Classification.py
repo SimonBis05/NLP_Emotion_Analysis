209c85285ec41f34a7560f8568e07b547a75ce55
YardenFadida <yardenfadida@gmail.com>
2025-11-12 17:34:45 -0400

    Better factorization and layout.

--- LR_Emotions_Classification.py
+++ LR_Emotions_Classification.py
@@ -5,56 +5,104 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score, classification_report
 
 
 class LogisticRegressionClassifier:
     def __init__(self, ds):
-        # Store dataset
+        """
+        Initialize parameters.
+        """
         self.ds = ds
+        self.label_names = None
+        self.count_vectorizer = None
+        self.mlb = None
+        self.mlb_classifier = None
+        self.train_counts = None
+        self.dev_counts = None
+        self.test_counts = None
+        self.binary_labels_train = None
+        self.binary_labels_dev = None
+        self.binary_labels_test = None
 
-        # Split datasets
-        train_set = ds['train']
-        dev_set = ds['validation']
-        test_set = ds['test']
 
-        # Extract texts and labels.
-        # Not storing train texts to save space.
-        train_texts, train_labels = train_set['text'], train_set['labels']
-        self.dev_texts, self.dev_labels= dev_set['text'], dev_set['labels']
-        self.test_texts, self.test_labels = test_set['text'], test_set['labels']
+    def _load_data(self):
+        """
+        Loading dataset and prepare it for training and testing.
+        """
+        try:
+            # Split datasets
+            train_set = self.ds['train']
+            dev_set = self.ds['validation']
+            test_set = self.ds['test']
 
-        # Get emotion names
-        self.label_names = train_set.features['labels'].feature.names
+            # Extract texts and labels.
+            self.train_texts, self.train_labels = train_set['text'], train_set['labels']
+            self.dev_texts, self.dev_labels= dev_set['text'], dev_set['labels']
+            self.test_texts, self.test_labels = test_set['text'], test_set['labels']
 
-        # Initialize tokenizer and vectorizer as attributes
-        # 28 emotions including neutral
-        print("Initializing CountVectorizer with custom tokenizer...")
-        self.count_vectorizer = CountVectorizer(analyzer=utilities.nltk_tokenizer)
-        self.mlb = MultiLabelBinarizer(classes=range(28))
+            # Get emotion names
+            self.label_names = train_set.features['labels'].feature.names
 
-        print("Starting feature extraction...")
-        self.train_counts = self.count_vectorizer.fit_transform(train_texts)
-        self.dev_counts = self.count_vectorizer.transform(self.dev_texts)
-        self.test_counts = self.count_vectorizer.transform(self.test_texts)
+        except Exception as e:
+            print(f"Error during loading data: {e}")
+            raise
+    
+    def _extract_features(self):
+        """
+        Extract features and labels from the dataset.
+        """
+        try:
+            # Initialize tokenizer and vectorizer
+            # 28 emotions including neutral
+            print("Initializing CountVectorizer with custom tokenizer...")
+            self.count_vectorizer = CountVectorizer(analyzer=utilities.nltk_tokenizer)
+            self.mlb = MultiLabelBinarizer(classes=range(28))
 
-        # Convert GoEmotions labels to binary indicator matrix for comparison
-        self.binary_labels_train = self.mlb.fit_transform(train_labels)
-        self.binary_labels_dev = self.mlb.transform(self.dev_labels)
-        self.binary_labels_test = self.mlb.transform(self.test_labels)
+            print("Starting feature extraction...")
+            self.train_counts = self.count_vectorizer.fit_transform(self.train_texts)
+            self.dev_counts = self.count_vectorizer.transform(self.dev_texts)
+            self.test_counts = self.count_vectorizer.transform(self.test_texts)
 
-        # Initialize classifier
-        # Initialize base classifier using saga solver
-        # saga should work well with large, sparse data and multi-label problems.
-        # Wrap with OneVsRestClassifier using the strategy One-vs-the-rest (OvR) multiclass.
-        base_lr = LogisticRegression(max_iter=1000, solver='saga', random_state=0)
-        self.mlb_classifier = OneVsRestClassifier(base_lr)
+            # Convert GoEmotions labels to binary indicator matrix for comparison
+            self.binary_labels_train = self.mlb.fit_transform(self.train_labels)
+            self.binary_labels_dev = self.mlb.transform(self.dev_labels)
+            self.binary_labels_test = self.mlb.transform(self.test_labels)
+
+        except Exception as e:
+            print(f"Error during feature extraction: {e}")
+            raise
+
+
+    def _initialize_classifier(self):
+        """
+        Initialize the multi-label classifier.
+        """
+        try:
+            # Initialize base classifier using saga solver
+            # saga should work well with large, sparse data and multi-label problems.
+            # Wrap with OneVsRestClassifier using the strategy One-vs-the-rest (OvR) multiclass.
+            base_lr = LogisticRegression(max_iter=1000, solver='saga', random_state=0)
+            self.mlb_classifier = OneVsRestClassifier(base_lr)
+        except Exception as e:
+            priunt(f"Error initializing classifier: {e}")
+            raise
 
 
     def train(self):
-        # Train multi-label classifier
-        self.mlb_classifier.fit(self.train_counts, self.binary_labels_train)
+        """
+        Train the classifier.
+        """
+        try:
+            self._load_data()
+            self._extract_features()
+            self._initialize_classifier()
+            print("Training classifier...")
+            self.mlb_classifier.fit(self.train_counts, self.binary_labels_train)
+
+        except Exception as e:
+            print(f"Error during training: {e}")
+            raise
 
 
 # A helper function to print out macro averaged P, R, F, accuracy, and relaxed accuracy calculations
